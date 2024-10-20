847ccca6859b2e4863931abc9af6fe5483df06ec
Percy Liang <percyliang@gmail.com>
2024-10-20 12:59:20 -0700

    use material UI, make descriptions work
    Original-Commit: https://github.com/marin-community/marin/commit/a364d1f1bd42522a983145fe72c7057839a62f66

--- marin/utilities/json_encoder.py
+++ marin/utilities/json_encoder.py
@@ -1,6 +1,9 @@
 import json
 import logging
+import jax
+from jax.numpy import float32, bfloat16
 from datetime import timedelta
+from pathlib import Path
 
 logger = logging.getLogger("ray")
 
@@ -9,6 +12,10 @@ class CustomJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, timedelta):
             return {"days": obj.days, "seconds": obj.seconds, "microseconds": obj.microseconds}
+        if isinstance(obj, Path):
+            return str(obj)
+        if obj in (float32, bfloat16):
+            return str(obj)
         try:
             return super().default(obj)
         except TypeError:

e19122ba3200c91db928bb03b134ca6c99f1215d
Abhinav Garg <abhinavg@stanford.edu>
2024-11-19 01:12:48 -0600

    Try adding new stuff to step status
    Original-Commit: https://github.com/marin-community/marin/commit/d745a2e99ce8f9b170dba2c63fd5359f90bf872d

--- marin/execution/status_actor.py
+++ marin/execution/status_actor.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import json
 from dataclasses import asdict
 from typing import List
@@ -8,7 +8,7 @@ import ray
 from ray import ObjectRef
 
 from marin.execution.executor_step_status import get_status_path, ExecutorStepEvent, read_events, is_failure, \
-    STATUS_SUCCESS
+    STATUS_SUCCESS, append_status_event
 
 
 @ray.remote
@@ -25,9 +25,7 @@ class StatusActor:
         elif reference is not None:
             # Status is being updated, We need to write this to GCP too
             reference = self.value_to_reference_status.get(output_path, (None, None))[1]
-
-
-
+            append_status_event(output_path, executor_step_event)
             status = executor_step_event.status
 
         elif executor_step_event is None:
@@ -35,14 +33,7 @@ class StatusActor:
 
         else:
             status = executor_step_event.status
-            path = get_status_path(output_path)
-            events = read_events(path)
-            events.append(executor_step_event)
-            # Note: gcs files are immutable so can't append, so have to read everything.
-            with fsspec.open(path, "w") as f:
-                for event in events:
-                    print(json.dumps(asdict(event)), file=f)
-
+            append_status_event(output_path, executor_step_event)
 
         self.value_to_reference_status[output_path] = (status, reference)
 
@@ -52,18 +43,17 @@ class StatusActor:
         date = datetime.now().isoformat()
         event = ExecutorStepEvent(date=date, status=status, message=message, ray_task_id=ray_task_id)
         self._add_status_and_reference(output_path, event, None)
-        # self.value_to_reference_status[output_path] = (status, self.value_to_reference_status[output_path][1])
 
     def add_update_reference(self, output_path: str, reference: ObjectRef):
-        self.value_to_reference_status[output_path] = (self.value_to_reference_status.get(output_path, (None, None))[0],
-                                                                                          reference)
+        self._add_status_and_reference(output_path, None, reference)
 
     def get_status_and_reference(self, output_path: str) -> tuple[str,ObjectRef]:
         return self.value_to_reference_status[output_path]
 
     def get_status(self, output_path: str) -> str | None:
-        # (If a key is present in the ACTOR then we return the status else we go to GCP and check.
-        # If it’s SUCCESS or FAILED, it’s true status, else it’s a stale status and we don’t consider it.
+        # If a key is present in the ACTOR then we return the status else we go to GCP and check,
+        # if it’s SUCCESS or FAILED, it’s true status, else it’s a stale status and we don’t consider it and return None
+
         if output_path in self.value_to_reference_status:
             return self.value_to_reference_status[output_path][0]
         else:
@@ -71,6 +61,8 @@ class StatusActor:
             events = read_events(status_path)
             if len(events) > 0:
                 if is_failure(events[-1].status) or events[-1].status == STATUS_SUCCESS:
+                    self.value_to_reference_status[output_path] = (events[-1], None)
                     return events[-1].status
-
-        return None
+            else: # No status file, so it's a new step
+                self.value_to_reference_status[output_path] = (None, None)
+                return None

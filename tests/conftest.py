b854f6389c301f96eae7d5bdb09561b0e98ce650
Christopher Chou <emu4yolife@gmail.com>
2025-04-23 04:54:44 +0000

    Remove bad test
    Original-Commit: https://github.com/marin-community/marin/commit/9461c539e2a7dc023779c67326a027184810fb16

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from marin.evaluation.evaluators.evaluator import ModelConfig
 model_config = ModelConfig(
     name="test-llama-200m",
     path="gs://marin-us-east5/gcsfuse_mount/perplexity-models/llama-200m",
-    engine_kwargs={"enforce_eager": True, "max_model_len": 2048},
+    engine_kwargs={"enforce_eager": True, "max_model_len": 1024},
 )
 
 

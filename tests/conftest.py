0ad82713ad3be85b14aa79e7f14830af4e1284f1
Christopher Chou <emu4yolife@gmail.com>
2025-05-11 19:03:39 +0000

    Address comments and add test for ray data batch llm inference
    Original-Commit: https://github.com/marin-community/marin/commit/f83376c8a80b5c7768238806a55d0da426136399

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import pytest
@@ -5,11 +6,46 @@ import ray
 
 from marin.evaluation.evaluators.evaluator import ModelConfig
 
-model_config = ModelConfig(
-    name="test-llama-200m",
-    path="gs://marin-us-east5/gcsfuse_mount/perplexity-models/llama-200m",
-    engine_kwargs={"enforce_eager": True, "max_model_len": 1024},
-)
+default_engine_kwargs = {"enforce_eager": True, "max_model_len": 1024}
+
+large_model_engine_kwargs = {"max_model_len": 1024, "tensor_parallel_size": 8}
+
+default_generation_params = {"max_tokens": 16}
+
+DEFAULT_BUCKET_NAME = "marin-us-east5"
+DEFAULT_DOCUMENT_PATH = "documents/test-document-path"
+
+
+@pytest.fixture(scope="module")
+def model_config():
+    config = ModelConfig(
+        name="test-llama-200m",
+        path="gs://marin-us-east5/gcsfuse_mount/perplexity-models/llama-200m",
+        engine_kwargs=default_engine_kwargs,
+        generation_params=default_generation_params,
+    )
+    yield config
+    config.destroy()
+
+
+@pytest.fixture
+def gcsfuse_mount_model_path():
+    return "/opt/gcsfuse_mount/perplexity-models/llama-200m"
+
+
+@pytest.fixture
+def gcsfuse_mount_llama_70b_model_path():
+    return "/opt/gcsfuse_mount/models/meta-llama--Llama-3-3-70B-Instruct"
+
+
+@pytest.fixture
+def gcsfuse_mount_llama_8b_model_path():
+    return "/opt/gcsfuse_mount/models/meta-llama--Llama-3-1-8B-Instruct"
+
+
+@pytest.fixture(scope="module")
+def test_file_path():
+    return "gs://marin-us-east5/documents/chris-test/test_50.jsonl.gz"
 
 
 @pytest.fixture
@@ -22,6 +58,9 @@ def current_date_time():
 
 @pytest.fixture(scope="module")
 def ray_tpu_cluster():
-    ray.init(resources={"TPU": 8, "TPU-v6e-8-head": 1}, num_cpus=120, ignore_reinit_error=True)
+    if os.getenv("START_RAY_TPU_CLUSTER") == "true":
+        ray.init(resources={"TPU": 8, "TPU-v6e-8-head": 1}, num_cpus=120, ignore_reinit_error=True)
+    else:
+        ray.init("auto", ignore_reinit_error=True)
     yield
     ray.shutdown()

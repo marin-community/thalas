eda498c8ecbe04edb9401245ad3ae8e0acdddabe
Ryan Williams <ryan.williams@openathena.ai>
2025-09-06 15:22:39 -0400

    Export more members used by Marin

--- src/thalas/__init__.py
+++ src/thalas/__init__.py
@@ -16,10 +16,17 @@ __version__ = "0.1.0"
 
 from .execution import (
     Executor,
+    ExecutorMainConfig,
     ExecutorStep,
     InputName,
     OutputName,
+    THIS_OUTPUT_PATH,
     VersionedValue,
+    ensure_versioned,
     executor_main,
+    get_executor_step,
+    output_path_of,
+    this_output_path,
+    unwrap_versioned_value,
     versioned,
 )
